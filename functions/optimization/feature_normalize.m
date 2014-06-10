bb8bb7677d6bb58bab8d278d0e8321c49b9d5e3f
ryanlogs <ryanlogs@gmail.com>
2014-06-10 20:39:00 +0530

    use apply pca now

--- functions/optimization/feature_normalize.m
+++ functions/optimization/feature_normalize.m
@@ -1,4 +1,4 @@
-function [X_norm, mu, sigma] = featureNormalize(X)
+function [X_norm, mu, sigma] = feature_normalize(X)
 %FEATURENORMALIZE Normalizes the features in X 
 %   FEATURENORMALIZE(X) returns a normalized version of X where
 %   the mean value of each feature is 0 and the standard deviation
--- functions/optimization/pca.m
+++ functions/optimization/pca.m
@@ -8,15 +8,17 @@ function [Z, U] =  pca(X_norm)
 	[ U, S, V] = svd(Sigma);	
 
 	% choose the least k, from n features such information is preserved
-	k = 1;
+	k = 0;
 	rating = 0;
 	diagonal = diag(S);
 	
 	while (rating < 0.99 && n ~= k)
-		rating = sum(diagonal(1:k)) ./ sum(diagonal);
-		disp(sprintf('Rating for %d features = %f',k,rating));
 		k = k + 1;
+		rating = sum(diagonal(1:k)) ./ sum(diagonal);
+		disp(sprintf('Rating for %d features = %f',k,rating));	
 	end
+	
+	disp(sprintf('\nInput reduced to %d features...\n',k));
 	
 	Z = X_norm * U(:,1:k);
 
