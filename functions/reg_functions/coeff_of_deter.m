e281b21e9862dad55be5aaae13983db6f2fee41b
ryanlogs <ryanlogs@gmail.com>
2014-06-18 21:46:17 +0530

    added coeff fun

---
+++ functions/reg_functions/coeff_of_deter.m
@@ -0,0 +1,12 @@
+function [r2] = coeff_of_deter(X, Y)
+	
+	m = size(X,1);
+	
+	X_mean = mean(X);
+	Y_mean = mean(Y);
+	
+	X_std = std(X);
+	Y_std = std(Y);
+	
+	r2 = (sum((X - X_mean) .* (Y - Y_mean)) ./ m ./(X_std*Y_std))^2
+end
