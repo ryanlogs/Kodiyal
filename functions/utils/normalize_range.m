d309a05799f0637c6ff3df1d06bd6b54693044f7
jamesshocker <jamesshocker@gmail.com>
2014-06-20 22:52:16 +0800

    nn reg full data

--- functions/utils/normalize_range.m
+++ functions/utils/normalize_range.m
@@ -3,7 +3,8 @@ function [A_norm] = normalise(A, r_min, r_max)
 	ratio = (r_max - r_min) ./ (max(A) - min(A));
 	
 	% divide by ratio
-	A_norm = bsxfun(@times, A - min(A), ratio);
+	A_norm = bsxfun(@minus, A, min(A));
+	A_norm = bsxfun(@times, A, ratio);
 	
 	% addition rmin
 	A_norm = bsxfun(@plus, A_norm, r_min);
